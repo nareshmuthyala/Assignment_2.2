59602732c4a0da10e0eb3c3a8921163bc95d4f2b
Naresh Muthyala <nareshmuthyala1@gmail.com>
2018-03-02 08:08:05 +0530

    updated to single while loop

--- Assignment_2.2.py
+++ Assignment_2.2.py
@@ -1,8 +1,10 @@
-i=1
-while(i<=5 and i>0):
-    print(i*'* ')
-    i=i+1
-    if(i==5):
-        while (i <= 5 and i > 0):
-            print(i * '* ')
-            i = i - 1
+i = 1
+j = 1
+flag = True
+while j <= 10:
+    j = j + 1
+    print(i * '* ')
+    if (j <= 5):
+        i = i + 1
+    else:
+        i = i - 1
